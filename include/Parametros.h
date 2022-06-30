858bdd9351800eee2490e156039b92f807ebaceb
jzar21 <juanfmontilla21@gmail.com>
2022-06-30 17:21:46 +0200

    Añadido main y parametros.h

---
+++ include/Parametros.h
@@ -0,0 +1,7 @@
+#ifndef PARAMETROS_H
+#define PARAMETROS_H
+
+int ancho_pantalla = 400;
+int largo_pantalla = 800;
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <raylib.h>
+#include "Parametros.h"
+#include "Pelota.h"
+#include "Rectangulo.h"
+
+int main(){
+    InitWindow(ancho_pantalla, largo_pantalla, "hollaaaa");
+    return 0;
+}

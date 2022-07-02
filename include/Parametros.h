114808523e855352729da74c2d8bc3b789d26f36
jzar21 <juanfmontilla21@gmail.com>
2022-07-02 18:15:26 +0200

    la pelota sale aleatoria

--- include/Parametros.h
+++ include/Parametros.h
@@ -6,10 +6,9 @@ int ancho = 1000;
 int largo = 600;
 
 //variables de juego
-float v_pelota = 0.3f;
 float v_barra = 0.2f;
 
-int t_pelota = 20;
+int t_pelota = 10;
 int alto_barra = 90;
 int ancho_barra = 20;
 int posx_ini_izd = 20;
--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,20 @@
 #include "Rectangulo.h"
 #include "Pintar.h"
 #include "F_Auxiliares.h"
-
+#include <random>
+#include <time.h>
 
 int main(){
     InitWindow(ancho, largo, "Mi Pong");
+    std::srand(time(NULL));
+    float v_y = (float)(-2 + rand() % 1)/10;
+    float v_x = (float)(-2 + rand() % 1)/10;
 
-    Pelota pelota(10, 200, t_pelota, v_pelota, v_pelota);
-
+    Pelota pelota(t_pelota, ancho / 2, largo / 2, v_x, v_y);
     Rectangulo r_izd(alto_barra, ancho_barra , posx_ini_izd, posy_ini, 0, v_barra);
-
     Rectangulo r_dch(alto_barra, ancho_barra, posx_ini_dch , posy_ini, 0, v_barra);
             
+
     while(!WindowShouldClose()){
         pelota.Mover(ancho, largo);
         pelota.RebotaBordes(ancho, largo);
